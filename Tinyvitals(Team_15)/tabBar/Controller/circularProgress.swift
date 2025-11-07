75b4b3eb084b307c563df28cf89c392ae49d5cf8
Vineeth Aitha <vineethaitha1234@gmail.com>
2025-11-07 12:44:22 +0530

    worked on homeScreen progress bar

---
+++ Tinyvitals(Team_15)/tabBar/Controller/circularProgress.swift
@@ -0,0 +1,20 @@
+//
+//  circularProgress.swift
+//  Tinyvitals(Team_15)
+//
+//  Created by user70 on 07/11/25.
+//
+
+import UIKit
+
+class circularProgress: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
